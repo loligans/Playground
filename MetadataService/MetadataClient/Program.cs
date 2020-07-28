954acd50e52ddc1382395988935447dd8a0cb44b
Ethan Lindemann-Michael <ethan.lindemann@outlook.com>
2020-07-28 00:02:16 -0700

    metadata initial commit

---
+++ MetadataService/MetadataClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetadataClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
