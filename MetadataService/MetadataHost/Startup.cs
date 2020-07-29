badc9db451799e2ad1ab1f0d4f50f02a2e93d6d6
Ethan L <loligans@Ethans-MacBook-Pro.local>
2020-07-28 19:47:25 -0700

    organizing startup

--- MetadataService/MetadataHost/Startup.cs
+++ MetadataService/MetadataHost/Startup.cs
@@ -15,33 +15,28 @@ namespace MetadataHost
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (!env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHsts();
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
