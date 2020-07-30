035ceebdf9f9474aaede05efacca62b0caa8a379
Ethan Lindemann-Michael <ethan.lindemann@outlook.com>
2020-07-30 02:23:12 -0700

    Added efcore tool and removed some models since this is a demo

--- MetadataService/Metadata.Data/MetadataDbContext.cs
+++ MetadataService/Metadata.Data/MetadataDbContext.cs
@@ -9,28 +9,10 @@ namespace Metadata.Data
     public interface IMetadataDbContext
     {
         /// <summary>
-        /// The set of all <see cref="ImageMetadata"/> entities in the database.
-        /// </summary>
-        /// <value></value>
-        DbSet<ImageMetadata> ImageMetadatas { get; }
-
-        /// <summary>
-        /// The set of all <see cref="AudioMetadata"/> entities in the database.
-        /// </summary>
-        /// <value></value>
-        DbSet<AudioMetadata> AudioMetadatas { get; }
-
-        /// <summary>
         /// The set of all <see cref="FileMetadata"/> entities in the database.
         /// </summary>
         /// <value></value>
         DbSet<FileMetadata> FileMetadatas { get; }
-
-        /// <summary>
-        /// The set of all <see cref="VideoMetadata"/> entities in the database.
-        /// </summary>
-        /// <value></value>
-        DbSet<VideoMetadata> VideoMetadatas { get; }
     }
 
     /// <inheritdoc cref="IMetadataDbContext" />
@@ -39,16 +21,7 @@ namespace Metadata.Data
         /// <inheritdoc cref="DbContext"/>
         public MetadataDbContext(DbContextOptions<MetadataDbContext> options) : base(options) { }
 
-        /// <inheritdoc cref="IMetadataDbContext.ImageMetadatas" />
-        public DbSet<ImageMetadata> ImageMetadatas { get; set; }
-
-        /// <inheritdoc cref="IMetadataDbContext.AudioMetadatas" />
-        public DbSet<AudioMetadata> AudioMetadatas { get; set; }
-
         /// <inheritdoc cref="IMetadataDbContext.FileMetadatas" />
         public DbSet<FileMetadata> FileMetadatas { get; set; }
-
-        /// <inheritdoc cref="IMetadataDbContext.VideoMetadatas" />
-        public DbSet<VideoMetadata> VideoMetadatas { get; set; }
     }
 }
--- MetadataService/Metadata.Data/Models/FileMetadata.cs
+++ MetadataService/Metadata.Data/Models/FileMetadata.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System;
 
 namespace Metadata.Data.Models
@@ -8,9 +9,34 @@ namespace Metadata.Data.Models
     public class FileMetadata
     {
         /// <summary>
-        ///
+        /// the unique identifier of this record
         /// </summary>
-        /// <value></value>
         public Guid Id { get; set; }
+        /// <summary>
+        /// The type of file.
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// The path to the file.
+        /// </summary>
+        [StringLength(384)]
+        public string Path { get; set; }
+        /// <summary>
+        /// A short description of the file.
+        /// </summary>
+        [StringLength(256)]
+        public string Description { get; set; }
+        /// <summary>
+        /// The date the file was created
+        /// </summary>
+        public DateTime Created { get; set; }
+        /// <summary>
+        /// The last time the file was modified
+        /// </summary>
+        public DateTime Modified { get; set; }
+        /// <summary>
+        /// The file size in bytes
+        /// </summary>
+        public long Size { get; set; }
     }
 }
